786df4d6ca478efeb7ad9654e519d28ec934d92e
Sasha Joseph <sasha@fuzzproductions.com>
2015-06-12 10:07:33 -0400

    Adding safe with

--- src/Eloquent/Model.php
+++ src/Eloquent/Model.php
@@ -4,6 +4,7 @@ namespace Fuzz\Data\Eloquent;
 
 use Carbon\Carbon;
 use Fuzz\Data\Schema\SchemaUtility;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
 abstract class Model extends Eloquent
@@ -26,9 +27,26 @@ abstract class Model extends Eloquent
 	}
 
 	/**
-	 * Unhide hidden properties from the model via a public interface.
+	 * Remove appended properties to the model via a public interface.
 	 *
 	 * @param string|array $appends
+	 * @return static
+	 */
+	public function removeAppends($appends)
+	{
+		if (is_string($appends)) {
+			$appends = func_get_args();
+		}
+
+		$this->appends = array_diff($this->appends, $appends);
+
+		return $this;
+	}
+
+	/**
+	 * Unhide hidden properties from the model via a public interface.
+	 *
+	 * @param string|array $hidden
 	 * @return static
 	 */
 	public function removeHidden($hidden)
@@ -40,6 +58,58 @@ abstract class Model extends Eloquent
 		$this->hidden = array_diff($this->hidden, $hidden);
 
 		return $this;
+	}
+
+	/**
+	 * "Safe" version of with eager-loading.
+	 *
+	 * Checks if relations exist before loading them.
+	 *
+	 * @param \Illuminate\Database\Eloquent\Builder $builder
+	 * @param string|array                          $relations
+	 */
+	public function scopeSafeWith(Builder $builder, $relations)
+	{
+		if (is_string($relations)) {
+			$relations = func_get_args();
+			array_shift($relations);
+		}
+
+		// Loop through all relations to check for valid relationship signatures
+		foreach ($relations as $name => $constraints) {
+			// Constraints may be passed in either form:
+			// 2 => 'relation.nested'
+			// or
+			// 'relation.nested' => function() { ... }
+			$constraints_are_name = is_numeric($name);
+			$relation_name        = $constraints_are_name ? $constraints : $name;
+
+			// Expand the dot-notation to see all relations
+			$nested_relations     = explode('.', $relation_name);
+			$model                = $builder->getModel();
+
+			foreach ($nested_relations as $index => $relation) {
+				if (method_exists($model, $relation)) {
+					// Iterate through relations if they actually exist
+					$model = $model->$relation()->getRelated();
+				} elseif ($index > 0) {
+					// If we found any valid relations, pass them through
+					$safe_relation = implode('.', array_slice($nested_relations, 0, $index));
+					if ($constraints_are_name) {
+						$relations[$name] = $safe_relation;
+					} else {
+						unset($relations[$name]);
+						$relations[$safe_relation] = $constraints;
+					}
+				} else {
+					// If we didn't, remove this relation specification
+					unset($relations[$name]);
+					break;
+				}
+			}
+		}
+
+		return $this->with($relations);
 	}
 
 	/**
